d0bf82ae582129832ab4d84847911c3cc12554ab
ivla <ivlabush@gmail.com>
2021-02-22 13:51:52 +0200

    BTC-012: sliders && HW

---
+++ lesson08_hw/js/slider.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $('.main-slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1
+    })
+
+    $('.section2-mixed-lower').slick({
+
+    })
+
+    $('.modal-open').on("click", function () {
+        $('.modal').fadeIn()
+    })
+
+    $('.modal-close').on("click", function () {
+        $('.modal').fadeOut()
+    })
+})
